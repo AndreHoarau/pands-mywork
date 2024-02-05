59ed3ea2ead5d0e5504a2095a90fe33b02e71812
Andre Hoarau <andgus95@gmail.com>
2024-02-05 11:57:08 +0000

    extra questions from lab week02

---
+++ week02/extraquestion18.py
@@ -0,0 +1,5 @@
+# program for some math
+# author: Andre Hoarau
+x= 21-4
+print(x)
+print(2==3)
---
+++ week02/nameandAge.py
@@ -0,0 +1,5 @@
+# this is a program that will rean in a name and age and output hello your age is x
+# author andre hoarau
+name = input('What is your name?')
+age = int(input ('What is your age?'))
+print (f'Hello {name},\t you age is {age}.')
