58c5fd8e692477034a498c73cbf48b8bb647df99
Andre Hoarau <andgus95@gmail.com>
2024-02-21 15:31:34 +0000

    finsihed the first half of week 4 labs extra

---
+++ week04-flow/lab4.1.4 Extra2.py
@@ -0,0 +1,9 @@
+# a program that will ask a user to keep entering a number until they have entered minus 1
+# author Andre Hoarau
+
+number = float(input(" type in a number:"))
+while number!= -1:
+    print ("try another number")
+    number = float(input("enter another number:"))
+
+    print("thats the number!")
