7e01a40984728e0faf5b8243350750c7dea149b4
Andre Hoarau <andgus95@gmail.com>
2024-02-26 19:10:14 +0000

    Labs as far as got

---
+++ week05-datastructures/labs5.1.1 quiz.py
@@ -0,0 +1,42 @@
+#Look at this code and answer the questions below, answers at the end of the lab sheet
+# Auiz taker: Andre Hoarau
+numberOfQuestions = 5
+averageAge = 23.4
+debugMode = True
+name = "joe"
+ages = []
+months = ('Jan', 'Feb', 'Mar')
+book = {}
+stuff = [ 12 , 'Fred', False, {}]
+someone = dict(firstname = "joe")
+me = {
+"firstName" : "Andrew",
+"teaching" : [{
+"courseName" : "programming",
+"semester" : 1
+},{
+"courseName" : "Data Representation",
+"semester" : 2
+}
+]
+}
+
+#Questions
+#What are the variable types of the following variables in the code above
+#a. numberOfQuestions = 5 int
+#b. averageAge = 23.4 float
+#c. debugMode= True boolean
+#d. name= "Joe" str
+#e. ages list 
+#f. months tuple
+#g. months[1] Jan Str
+#h. book Dict
+#i. stuff List
+#j. stuff[2] Str
+#k. someone dict
+#l. someone["firstname"] str
+#m. me dict
+#n. me["teaching"] list
+#o. me["teaching"][0]["semester"] int
+#p is a trick question look at it carefully 
+#p. me["teaching"][0]["coursename"] error as there is a capital N in the dictionary and the question asked for lowercase n
---
+++ week05-datastructures/labs5.1.2 months.py
@@ -0,0 +1,6 @@
+# program that has a tuple with the months of the year with another tuple for the summer months and prints out the months one at a time 
+# Author: Andre Hoarau
+months = ("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December")
+summertime= months[4:7]
+for month in summertime:
+    print (month)
