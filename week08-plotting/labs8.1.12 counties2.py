3ae240901c771b9e2a139d675ae1399b1b42a1c6
Andre Hoarau <andgus95@gmail.com>
2024-03-23 12:29:19 +0000

    Finished the lab still catching up need to go back to do the extra

--- week08-plotting/labs8.1.12 counties2.py
+++ week08-plotting/labs8.1.12 counties2.py
@@ -1,9 +1,9 @@
-# A Programme that takes a list of counties 5 and make  a pie chart of the counties.
+#Modify the previous programme to make it a bar chart
 # Author Andre Hoarau
 import numpy as np
 import matplotlib.pyplot as plt
 listofcounties= ["Dublin", "Kildare", "Galway", "Mayo", "Sligo"]
 sizes =[15,30,45,10,60]
 
-plt.pie(sizes, labels= listofcounties)
+plt.bar(listofcounties,sizes)
 plt.show()
